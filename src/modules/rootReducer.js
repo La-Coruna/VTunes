a4f59b09bce0b89ce90b0da77c19f84ec843ea88
Park Jongyun <65222636+La-Coruna@users.noreply.github.com>
2023-11-16 23:59:53 +0900

    reducer에 playlistsSlice추가

--- src/modules/rootReducer.js
+++ src/modules/rootReducer.js
@@ -1,9 +1,13 @@
 //루트 리듀서 만들기
-import {combineReducers} from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
 import musicController from './musicController';
+import playlistsSlice from './playlistsSlice';
 
 const rootReducer = combineReducers({
-  musicController
+  playlists: playlistsSlice.reducer,
+  musicController,
 });
 
 export default rootReducer;
+export const {createPlaylist} = playlistsSlice.actions
+export const {deletePlaylist} = playlistsSlice.actions
